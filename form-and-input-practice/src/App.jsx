9945179ef96ae2b061c73a549517adb1117b240a
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2023-08-03 13:23:22 +0200

    Form and input validation - handling basic problems

---
+++ form-and-input-practice/src/App.jsx
@@ -0,0 +1,12 @@
+import SimpleInput from "./components/SimpleInput";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <SimpleInput />
+    </div>
+  );
+}
+
+export default App;
