51e5e12e40e6a80d9ecab334adb93ebfd635cb14
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2023-08-10 10:49:53 +0200

    Added a way to store and fetch cart to/from db and display notification of its status

--- redux-shop-advanced/src/store/ui-slice.jsx
+++ redux-shop-advanced/src/store/ui-slice.jsx
@@ -1,12 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialUiState = { showCart: false };
+const initialUiState = { showCart: false, notification: null };
 const uiSlice = createSlice({
   name: "ui",
   initialState: initialUiState,
   reducers: {
     toggleCart(state) {
       state.showCart = !state.showCart;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
     },
   },
 });
